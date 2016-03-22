147cb7e32a10ed45bc5a03bf63885b6daf63cb9a
vasilesk <i@vasilesk.ru>
2016-03-22 18:38:31 +0300

    type system added to Main, minor changes added to Spec

--- app/Main.hs
+++ app/Main.hs
@@ -7,3 +7,15 @@ main = do
   putStrLn "Enter your name:"
   s <- getLine
   putStrLn (someFunc s)
+
+type World_object = (Checkers, State, Alert_message)
+type Alert_message = String
+type State = Int
+type Checkers = ([Checker], [Checker]) -- white and black checkers
+type Checker = (Checkerboard_pos, Is_alive, Is_king)
+type Checkerboard_pos = Int
+type Is_alive = Bool
+type Is_king = Bool
+
+
+-- world :: (checkers, state, )
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,29 +8,46 @@ import System.IO.Unsafe
 main :: IO ()
 main
  = display
-        (InWindow
-	       "Hello World" 	 -- window title
-		(400, 150) 	 -- window size
-		(10, 10)) 	 -- window position
-	white			 -- background color
- 	(Pictures all_pictures)			 -- picture to display
+         (InWindow
+         "Hello World"  -- window title
+    (800, 600)   -- window size
+    (10, 10))  -- window position
+  white -- background color
+  (Pictures all_pictures) -- picture to display
 
-all_pictures::[Picture]
+all_pictures :: [Picture]
 all_pictures
- = [picture_bmp, picture1, picture2]
+  = [picture_bmp, picture1, picture2]
 
-picture1::Picture
+picture1 :: Picture
 picture1
-	= Translate (-170) (-20) -- shift the text to the middle of the window
-	$ Scale 0.5 0.5		 -- display it half the original size
-	$ Text "Hello World"	 -- text to display
+  = Translate (-170) (-20) -- shift the text to the middle of the window
+  $ Scale 0.5 0.5  -- display it half the original size
+  $ Color red
+  $ Text "Hello World"  -- text to display
 
-picture2::Picture
+picture2 :: Picture
 picture2
-  	= Translate (0) (-10) -- shift the text to the middle of the window
-  	$ Scale 1 1		 -- display it half the original size
-  	$ Circle 15	 -- text to display
+   = Translate (0) (-10)
+   $ Scale 1 1
+   $ ThickCircle 15 15
 
-picture_bmp:: Picture
+picture_bmp :: Picture
 picture_bmp
-  	= unsafePerformIO (loadBMP "data/bg.bmp")
+  = Translate (0) (-10) -- shift the text to the middle of the window
+  $ Scale 2 2  -- display it double the original size
+  $ unsafePerformIO (loadBMP "data/bg.bmp")
+
+cell :: Int -> Float -> Float -> Picture -- cell_type -> x -> y -> result picture
+cell
+ _ x y = Translate (x) (y)
+        $ Scale 1 1
+        $ unsafePerformIO (loadBMP "data/bg.bmp")
+
+
+add_row :: [Picture] -> Int -> Float -> Float -> [Picture] -- input picture list -> iterations left -> x -> y -> result picture list
+add_row pic_list 0 _ _ = pic_list
+add_row pic_list n x y = (cell 0 x y):(add_row (pic_list) (n - 1) (x + 100) y)
+
+-- add_board :: [Picture] -> [Picture]
+-- add_promt :: [Picture] -> [Picture]
