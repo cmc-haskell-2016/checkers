5e907584475befd244ad56bd9cbaf734236562c8
Maria Tereshina <tereshinams@rambler.ru>
2016-04-02 13:12:21 +0300

    is_there_checker func in src/World_processing.hs added

--- src/World_processing.hs
+++ src/World_processing.hs
@@ -8,7 +8,11 @@ create_checkers_object = undefined
 
 -- Testing if there is a checker of the player in the board position
 is_there_checker :: Checkerboard_pos -> Checkers -> State -> Bool
-is_there_checker = undefined
+is_there_checker pos checker_set (stateId, _, _) = 
+                                 (any (\(checkerPos, isAlive, _) -> pos == checkerPos && isAlive) (id_func checker_set))
+                                 --states whether there is a living checker with a given position of an appropriate colour
+                                           where id_func = if stateId == 1 then fst else snd 
+                                           --makes tuple extraction function depending on stateId
 
 -- List of possible moves from the position given as the first argument
 possible_moves :: Checkerboard_pos -> Checkers -> State -> [Checkerboard_pos]
